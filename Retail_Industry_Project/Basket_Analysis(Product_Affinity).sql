3f68b20f977707c3ff26ffd8348c5c10f90437b2
Harshith Narasimhamurthy <106474298+Harshith69@users.noreply.github.com>
2025-05-24 17:23:40 +0530

    Update Basket_Analysis(Product_Affinity).sql

--- Retail_Industry_Project/Basket_Analysis(Product_Affinity).sql
+++ Retail_Industry_Project/Basket_Analysis(Product_Affinity).sql
@@ -1,5 +1,5 @@
 -- Finding products which are frequently purchased together (more than 10 times)
-SELECT p1.product_name AS product1,p 2.product_name AS product2, COUNT(*) AS times_purchased_together
+SELECT p1.product_name AS product1, p2.product_name AS product2, COUNT(*) AS times_purchased_together
 FROM sales s1
 JOIN sales s2 ON s1.sale_id = s2.sale_id AND s1.product_id < s2.product_id
 JOIN products p1 ON s1.product_id = p1.product_id
